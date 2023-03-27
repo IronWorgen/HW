c4943ad91c049f7cfc13290189587d136af2c4af
ivasenko <ivasenko98@list.ru>
2023-03-27 19:50:53 +0300

    task 2 complete

---
+++ HW_sem9/task2/task2.cs
@@ -0,0 +1,29 @@
+﻿//  Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//  
+//  M = 1; N = 15 -> 120
+//  M = 4; N = 8. -> 30
+
+int SumBetweenMandN(int m, int n)
+{
+    int result = 0;
+    if (m < n)
+    {
+        if (m >= n)
+        {
+            return result = n;
+        }
+        result += m + SumBetweenMandN(m + 1, n);
+    }
+    else
+    {
+        if (m <= n)
+        {
+            return result = n;
+        }
+        result += n + SumBetweenMandN(m, n + 1);
+    }
+
+    return result;
+}
+
+Console.WriteLine(SumBetweenMandN(1, 15));
