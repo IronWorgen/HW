c28f47245bf2db1f55a5784d42c095e1fd9b8a69
ivasenko <ivasenko98@list.ru>
2023-03-27 20:02:37 +0300

    task 3 complete

---
+++ HW_sem9/task3/Program.cs
@@ -0,0 +1,24 @@
+﻿//      Задача 3: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//      Даны два неотрицательных числа m и n.
+//      m = 2, n = 3 -> A(m,n) = 9
+//      m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int number, int argument)
+{
+    if (number == 0)
+    {
+        return argument + 1;
+    }
+    else
+    {
+        if ((number != 0) && (argument == 0))
+        {
+            return Akkerman(number - 1, 1);
+        }
+        else
+        {
+            return Akkerman(number - 1, Akkerman(number, argument - 1));
+        }
+    }
+}
+Console.WriteLine(Akkerman(2, 3));
