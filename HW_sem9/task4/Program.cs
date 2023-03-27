a80b3674942faf539aa33e2c2716c68460c00be5
ivasenko <ivasenko98@list.ru>
2023-03-27 20:10:04 +0300

    task 4 complete

---
+++ HW_sem9/task4/Program.cs
@@ -0,0 +1,27 @@
+﻿//      Задача со звездочкой Найти второй максимум в массиве.
+//      4, 4, 3, 0, 2 => 3
+
+int SecondMax(int[] arr)
+{
+    int max1 = arr[0];
+    int max2 = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+            continue;
+
+        }
+        if (arr[i] > max2 & arr[i] != max1)
+        {
+            max2 = arr[i];
+        }
+
+    }
+    return max2;
+}
+
+int [] arr = {3, 5, 3, 0, 4};
+Console.WriteLine(SecondMax(arr));
